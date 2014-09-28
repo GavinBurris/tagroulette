4e9fe71a4773d1db4594af159d7978375756e6d5
johnburd <jburd@silverburd.com>
2014-09-28 15:18:49 -0500

    Moved body tag around content.

--- index.php
+++ index.php
@@ -4,22 +4,22 @@
 	<title>TagRoulette</title>
 	<link rel="stylesheet" type="text/css" href="./style/main.css">
 </head>
-<div id="header">
-	<h1>TagRoulette<sup>Beta</sup></h1>
-	<p>
-	TagRoulette is a new type of social media. Each member gets a pesonal account with every picture they have ever taken. Each friend will be ask to tag a friend and that friend to tag another.
-
-	</p>
-</div>
-<nav>
-	<ul>
-		<li><a href="#test">Test</a></li>
-		<li><a href="#test">Test</a></li>
-		<li><a href="#test">Test</a></li>
-		<li><a href="#test">Test</a></li>
-	</ul>
-</nav>
 <body>
-	<div id="info">
+	<div id="header">
+		<h1>TagRoulette<sup>Beta</sup></h1>
+		<p>
+		TagRoulette is a new type of social media. Each member gets a pesonal account with every picture they have ever taken. Each friend will be ask to tag a friend and that friend to tag another.
+
+		</p>
 	</div>
+	<nav>
+		<ul>
+			<li><a href="#test">Test</a></li>
+			<li><a href="#test">Test</a></li>
+			<li><a href="#test">Test</a></li>
+			<li><a href="#test">Test</a></li>
+		</ul>
+	</nav>
+		<div id="info">
+		</div>
 </body>
