eead8c82a104c32f762e3b1fde9d6ecc1ac257fa
johnburd <jburd@silverburd.com>
2014-09-29 17:34:06 -0500

    Updated mobile banner.

--- public_html/banner.php
+++ public_html/banner.php
@@ -1,10 +1,19 @@
 <div id="banner">
 	<link rel="stylesheet" type="text/css" href="./style/banner.css">
 	<ul>
-		<li><a href="./index.php">Home</a></li>
+		<li><a href="./home.php">Home</a></li>
 		<li><a href="#">Help</a></li>
 		<li><h1>Tag Roulette</h1></li>
 		<li><a href="#">About</a></li>
 		<li><a href="#">Profile</a></li>
+	</ul>
+</div>
+<div id="m_banner">
+	<h1>Tag Roulette</h1>
+	<l>
+		<li><a href="./home.php">Home</a></li>
+		<li><a href="./home.php">Profile</a></li>
+		<li><a href="./home.php">Home</a></li>
+		<li><a href="./home.php">Profile</a></li>
 	</ul>
 </div>
